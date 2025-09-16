d9f3e7833b45fe0308237a5df82fb426da30411c
hedwig <150428098+its-levi0sa@users.noreply.github.com>
2025-09-17 00:25:55 +0800

    Update final_evaluate.py

--- scripts/final_evaluate.py
+++ scripts/final_evaluate.py
@@ -15,8 +15,8 @@ from sklearn.metrics import confusion_matrix
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.aeye_model import AEyeModel
-from src.baseline_model import BaselineModel
-from src.data_utils import get_transforms, CataractDataset
+from src.baseline_model import MobileViT
+from src.data_utils import get_transforms, AlbumentationsDataset as CataractDataset
 
 def evaluate_single_model(model, dataloader, device):
     """Evaluates a single model on the provided dataloader."""
